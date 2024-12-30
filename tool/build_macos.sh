c51b612f958a8862c47af26d2d54adbf9d207d66
‎ <>
2024-12-30 09:45:58 +0700

    chaange database universe

--- tool/build_macos.sh
+++ tool/build_macos.sh
@@ -4,5 +4,5 @@ rustup target add aarch64-apple-darwin x86_64-apple-darwin
 cargo build --target aarch64-apple-darwin --features sqlcipher --release
 cargo build --target x86_64-apple-darwin --features sqlcipher --release
 
-lipo "target/aarch64-apple-darwin/release/libdatabase_universe.dylib" "target/x86_64-apple-darwin/release/libdatabase_universe.dylib" -output "libdatabase_universe_macos.dylib" -create
-install_name_tool -id @rpath/libdatabase_universe.dylib libdatabase_universe_macos.dylib
+lipo "target/aarch64-apple-darwin/release/libDatabaseUniverse.dylib" "target/x86_64-apple-darwin/release/libDatabaseUniverse.dylib" -output "libDatabaseUniverse_macos.dylib" -create
+install_name_tool -id @rpath/libDatabaseUniverse.dylib libDatabaseUniverse_macos.dylib
