aaa524e81895ec0e7e2f618d69bf3550fb788663
‎ <>
2024-12-30 14:07:15 +0700

    update

--- tool/build_wasm.sh
+++ tool/build_wasm.sh
@@ -1,3 +1,4 @@
 rustup target add wasm32-unknown-unknown
 cargo build --target wasm32-unknown-unknown --features sqlite --no-default-features -p database-universe-core --release
+ls -l -a target/wasm32-unknown-unknown/release/
 mv "target/wasm32-unknown-unknown/release/database_universe.wasm" "database_universe.wasm"
