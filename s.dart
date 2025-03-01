5d3364852c4343561410e08b0b1c5df144c8fd55
‎ <>
2025-03-01 17:27:37 +0700

    up

--- s.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-import 'package:general_lib/extension/dynamic.dart';
-
-void main(List<String> args) {
-  File("/home/galaxeus/.config/dart/pub-credentials.json")
-      .readAsStringSync()
-      .printPretty();
-}
